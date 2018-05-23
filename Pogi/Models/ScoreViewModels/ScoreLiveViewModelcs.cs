0c824dcee804580cf45804ce7f1a35d57514ff96
ngeslani63 <ngeslani@yahoo.com>
2018-05-22 22:22:33 -0400

    Live Scoring  Work 1

---
+++ Pogi/Models/ScoreViewModels/ScoreLiveViewModelcs.cs
@@ -0,0 +1,16 @@
+﻿using Pogi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pogi.Models.ScoreViewModels
+{
+    public class ScoreLiveViewModelcs
+    {
+        public Course Course { get; set; }
+        public TeeTime TeeTime { get; set; }
+        
+        List<Score> Scores { get; set; }
+    }
+}
